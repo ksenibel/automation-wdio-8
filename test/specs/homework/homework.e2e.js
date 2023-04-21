285db71c5727cf14bfd186247e278c6af95424bc
Ksenia Belousova <ksu_belousova@yahoo.com>
2023-04-21 19:22:31 +0200

    homework 1

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -2,7 +2,11 @@ describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
 
-        // sem vypracuj domácí úkol
+        await browser.reloadSession();
+
+        await browser.url('/registrace');
+
+        await browser.saveScreenshot('registrace_page.png')
 
     });
 
