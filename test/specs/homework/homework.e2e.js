be928ed9d2aeb889c790ff2c540f84201647400e
Ksenia Belousova <ksu_belousova@yahoo.com>
2023-05-09 20:21:02 +0200

    homework 3

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,13 +1,33 @@
 describe('Homework', async () => {
 
-    it('should open page and create screenshot', async () => {
+    it('should open page', async () => {
 
         await browser.reloadSession();
 
         await browser.url('/registrace');
 
-        await browser.saveScreenshot('registrace_page.png')
+        const idNameSelector = $('#name');
+        console.log(await idNameSelector.getHTML());
 
+        const idEmailSelector = $('#email');
+        console.log(await idEmailSelector.getHTML());
+
+        const idPasswordSelector = $('#password');
+        console.log(await idPasswordSelector.getHTML());
+
+        const idPasswordControlSelector = $('#password-confirm');
+        console.log(await idPasswordControlSelector.getHTML());
+
+        const buttonSelector = $('.btn-primary');
+        console.log(await buttonSelector.getHTML());
+
+        await idNameSelector.setValue('Name Surname');
+        await idEmailSelector.setValue('da-app.admin@czechitas.cz');
+        await idPasswordSelector.setValue('Czechitas123');
+        await idPasswordControlSelector.setValue('Czechitas123');
+        await buttonSelector.click();
+
+        await browser.pause(5000);
     });
 
 });
