6f506aa1cb8a706886a0177680727883cd49b5e6
Ksenia Belousova <ksu_belousova@yahoo.com>
2023-05-23 20:27:27 +0200

    Homework 4

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,16 +1,19 @@
 describe('Homework', async () => {
 
-    it('should open page', async () => {
-
+    beforeEach(async () => {
         await browser.reloadSession();
-
         await browser.url('/registrace');
+    });
 
-        const idNameSelector = $('#name');
-        console.log(await idNameSelector.getHTML());
+
+    it('Should check if the registration field is displayed correctly', async () => {
+
+        const nameField = $('#name')
+        console.log('Name field is displayed' + await nameField.getHTML());
+        console.log('Name field is enabled: ' + await nameField.isEnabled());
 
         const emailField = $('#email');
-        console.log('Email filed is displayed' + await emailField.getHTML());
+        console.log('Email field is displayed' + await emailField.getHTML());
         console.log('Email field is enabled: ' + await emailField.isEnabled());
 
         const passwordField = $('#password');
@@ -24,14 +27,77 @@ describe('Homework', async () => {
         const loginButtonSelector = $('.btn-primary');
         console.log('Login button text' + await loginButtonSelector.getHTML());
 
-        await idNameSelector.setValue('Name Surname');
+        await browser.pause(5000);
+    });
+
+    it('Should register the user with valid cridentials', async () => {
+        const nameField = $('#name');
+        const emailField = $('#email');
+        const passwordField = $('#password');
+        const passwordFieldControl = $('#password-confirm');
+        const loginButtonSelector = $('.btn-primary');
+
+        await nameField.setValue('Test Test');
+        await emailField.setValue('223456@email.cz');
+        await passwordField.setValue('Czech123');
+        await passwordFieldControl.setValue('Czech123');
+        await loginButtonSelector.click();
+
+        console.log('Email field is displayed: ' + await emailField.isDisplayed());
+        console.log('Password field is displayed: ' + await passwordField.isDisplayed());
+        console.log('Login button is displayed: ' + await loginButtonSelector.isDisplayed());
+
+        const registeredUser = $('.navbar-right').$('[data-toggle="dropdown"]');
+        console.log('Registered user is dispalyed ' + await registeredUser.getText());
+
+        await browser.pause(10000);
+    })
+
+    it('Should not register the user with existing email', async () => {
+
+        const nameField = $('#name');
+        const emailField = $('#email');
+        const passwordField = $('#password');
+        const passwordFieldControl = $('#password-confirm');
+        const loginButtonSelector = $('.btn-primary');
+
+        await nameField.setValue('Lišák Admin');
         await emailField.setValue('da-app.admin@czechitas.cz');
         await passwordField.setValue('Czechitas123');
         await passwordFieldControl.setValue('Czechitas123');
         await loginButtonSelector.click();
 
+        const toastMessage= $('.toast-message');
+        console.log('toast-message appeared ' + await toastMessage.getText());
+
+        const errorField = $('.invalid-feedback');
+        console.log('error message appeared ' + await errorField.getText());
+
         await browser.pause(5000);
     });
 
-});
+    it('Should not register the user with the wrong password', async () => {
 
+        const nameField = $('#name');
+        const emailField = $('#email');
+        const passwordField = $('#password');
+        const passwordFieldControl = $('#password-confirm');
+        const loginButtonSelector = $('.btn-primary');
+
+        await nameField.setValue('Test Test');
+        await emailField.setValue('11234@email.cz');
+        await passwordField.setValue('1234567890');
+        await passwordFieldControl.setValue('1234567890');
+        await loginButtonSelector.click();
+
+        const toastMessage= $('.toast-message');
+        console.log('toast-message appeared ' + await toastMessage.getText());
+
+        const errorField = $('.invalid-feedback');
+        console.log('error message appeared ' + await errorField.getText());
+
+        await browser.pause(5000);
+
+    });
+
+});
